6fd5c80c75647aa6c71cc330d221e34eb672618b
NoorMdKhan123 <86184257+NoorMdKhan123@users.noreply.github.com>
2021-10-24 03:07:44 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,13 +14,18 @@ namespace HashTableExample
             hash.Add("4", "to");
             hash.Add("5", "be");
 
-            string hash5 = hash.Get("5");
-
-
-            Console.WriteLine("5th index value : " + hash5);
-
+            string hash0 = hash.Get("0");
+            Console.WriteLine("0th index value : " + hash0);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1st index value : " + hash1);
             string hash2 = hash.Get("2");
             Console.WriteLine("2nd index value : " + hash2);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value : " + hash3);
+            string hash4 = hash.Get("4");
+            Console.WriteLine("2nd index value : " + hash4);
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value : " + hash5);
 
         }
     }
